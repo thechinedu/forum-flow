12f192e29947935ee51d2596c0a8c52aa354d5ec
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-08-10 09:58:04 +0200

    Add static homepage [status: complete]

--- apps/web/app/components/Home/Home.tsx
+++ apps/web/app/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import styles from './Home.module.css';
 
 import { Header } from '@/app/components/Header';
-import { Downvote, Upvote } from '@/app/components/Icons';
+import { Comment, Downvote, Upvote } from '@/app/components/Icons';
 import Image from 'next/image';
 
 export const Home = () => {
@@ -30,7 +30,8 @@ export const Home = () => {
           </div>
 
           <div className={styles.postContent}>
-            <p>Check out this awesome post</p>
+            <h4>Check out this awesome post</h4>
+            <p>Do check this out</p>
           </div>
 
           <div className={styles.postActions}>
@@ -44,7 +45,89 @@ export const Home = () => {
               </button>
             </div>
 
-            <div>9.7k comments</div>
+            <div className={styles.comments}>
+              <Comment /> 9.7k
+            </div>
+          </div>
+        </div>
+        <div className={styles.postContainer}>
+          <div className={styles.postDetails}>
+            <div className={styles.postDetailsUserCommunity}>
+              <Image
+                src="https://placebeard.it/24/24/notag"
+                alt="profile image"
+                width={24}
+                height={24}
+              />
+              <span>c/blog</span>
+              <span>•</span>
+              <span>1h</span>
+            </div>
+
+            <div className={styles.joinCommunity}>
+              <button>Join</button>
+            </div>
+          </div>
+
+          <div className={styles.postContent}>
+            <h4>Check out this awesome post</h4>
+            <p>Do check this out</p>
+          </div>
+
+          <div className={styles.postActions}>
+            <div className={styles.voteActions}>
+              <button>
+                <Upvote />
+              </button>
+              15k
+              <button>
+                <Downvote />
+              </button>
+            </div>
+
+            <div className={styles.comments}>
+              <Comment /> 9.7k
+            </div>
+          </div>
+        </div>
+        <div className={styles.postContainer}>
+          <div className={styles.postDetails}>
+            <div className={styles.postDetailsUserCommunity}>
+              <Image
+                src="https://placebeard.it/24/24/notag"
+                alt="profile image"
+                width={24}
+                height={24}
+              />
+              <span>c/blog</span>
+              <span>•</span>
+              <span>1h</span>
+            </div>
+
+            <div className={styles.joinCommunity}>
+              <button>Join</button>
+            </div>
+          </div>
+
+          <div className={styles.postContent}>
+            <h4>Check out this awesome post</h4>
+            <p>Do check this out</p>
+          </div>
+
+          <div className={styles.postActions}>
+            <div className={styles.voteActions}>
+              <button>
+                <Upvote />
+              </button>
+              15k
+              <button>
+                <Downvote />
+              </button>
+            </div>
+
+            <div className={styles.comments}>
+              <Comment /> 9.7k
+            </div>
           </div>
         </div>
       </main>
